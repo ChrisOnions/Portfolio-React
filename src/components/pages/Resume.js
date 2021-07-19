dae0a3161d57557ad360def34b82d433c4878359
Chris Onions <c.onions101@gmail.com>
2021-07-19 17:15:11 +1000

    Addeed skills to the resume page also added assets to the readme

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import resumeImg from './Capture.png'
 
 
+
 function Resume() {
   return (
 
@@ -10,13 +11,32 @@ function Resume() {
 
         <div>
           <h1>Resume / Cv</h1>
+          <h4>Skills</h4>
+          <div>
+            <ul> Computer Science applied to JavaScript</ul>
+            <ul>Design Patterns</ul>
+            <ul>Data Structures</ul>
+            <ul>Algorithms</ul>
+            <ul>Big O Notation</ul>
+            <ul></ul>Browser Based Technologies
+            <ul>HTML</ul>
+            <ul>CSS</ul>
+            <ul>JavaScript</ul>
+            <ul>jQuery</ul>
+            <ul>Responsive Design</ul>
+            <ul>Bootstrap</ul>
+            <ul>Handlebars</ul>
+            <ul>Local Storage, Session Storage, IndexedDB</ul>
+            <ul>React.js</ul>
+          </div>
           <button>Download</button>
         </div>
+
         <img class='resume' src={resumeImg} alt="resume"></img>
 
       </div>
-
     </div>
+
 
   )
 }
